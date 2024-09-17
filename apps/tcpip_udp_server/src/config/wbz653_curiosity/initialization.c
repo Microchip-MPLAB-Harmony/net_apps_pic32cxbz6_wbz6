806ac5e54ef2d8f9e1aba580e0ffddc35eebbb2a
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2024-09-17 11:48:41 +0530

    [MH3-98687] - Added ANNOUNCE to tcpip_udp_server application - wbz653_curiosity

--- apps/tcpip_udp_server/src/config/wbz653_curiosity/initialization.c
+++ apps/tcpip_udp_server/src/config/wbz653_curiosity/initialization.c
@@ -208,6 +208,11 @@ const TCPIP_ARP_MODULE_CONFIG tcpipARPInitData =
     .gratProbeCount     = TCPIP_ARP_GRATUITOUS_PROBE_COUNT,
 };
 
+/*** Announce Discovery Initialization Data ***/
+const TCPIP_ANNOUNCE_MODULE_CONFIG tcpipAnnounceInitData =
+{ 
+    0
+};
 
 /*** UDP Sockets Initialization Data ***/
 const TCPIP_UDP_MODULE_CONFIG tcpipUDPInitData =
@@ -316,6 +321,7 @@ const TCPIP_STACK_MODULE_CONFIG TCPIP_STACK_MODULE_CONFIG_TBL [] =
     {TCPIP_MODULE_ARP,              &tcpipARPInitData},             // TCPIP_MODULE_ARP
     {TCPIP_MODULE_UDP,              &tcpipUDPInitData},             // TCPIP_MODULE_UDP
     {TCPIP_MODULE_DHCP_CLIENT,      &tcpipDHCPInitData},            // TCPIP_MODULE_DHCP_CLIENT
+    {TCPIP_MODULE_ANNOUNCE,         &tcpipAnnounceInitData},        // TCPIP_MODULE_ANNOUNCE
     {TCPIP_MODULE_DNS_CLIENT,       &tcpipDNSClientInitData},       // TCPIP_MODULE_DNS_CLIENT
 
     {TCPIP_MODULE_COMMAND,          0},                             // TCPIP_MODULE_COMMAND,
@@ -543,9 +549,9 @@ void SYS_Initialize ( void* data )
 
     TC0_TimerInitialize();
 
-    EVSYS_Initialize();
-
     SERCOM0_USART_Initialize();
+
+    EVSYS_Initialize();
 
 
 
